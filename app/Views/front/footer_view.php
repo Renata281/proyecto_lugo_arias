1e255d0146179bb6958e67241817996223644994
Renata <lugorenata26@gmail.com>
2025-04-17 12:07:38 -0300

    cambios

--- app/Views/front/footer_view.php
+++ app/Views/front/footer_view.php
@@ -1,9 +1,20 @@
 </head>
 <footer>
+    
+<!--Informacion acerca de la pagina-->
+<ul>
+<li><a href="<?php echo base_url('comercializacion');?>">Comercialización</a></li>
+<li><a href="">Consultas</a></li>
+<li><a href="">Contactos</a></li>
+<li><a href="">Quienes somos</a></li>
+<li><a href="">Términos y usos</a></li>
+</ul>
+
+<!--Redes sociales-->
 
 
-
-
+<!--Derechos reservados-->
+<p>© 2025 V&V Pasteleria artesanal. Todos los derechos reservados</p>
 </footer>
 
 </body>
--- app/Views/front/nav_view.php
+++ app/Views/front/nav_view.php
@@ -1,7 +1,29 @@
 
 <header>
 <nav>
+<!--Logo-->
 
+<!--Informacion-->
+<ul class="nav nav-underline">
+<li class="nav-item">
+  <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+  </li>
+
+  <li class="nav-item">
+  <a class="nav-link" href="">Quienes somos</a>
+  </li>
+  
+  <li class="nav-item">
+  <a class="nav-link" href="">Comercialización</a>
+  </li>
+
+  <li class="nav-item">
+    <a class="nav-link" href="">Contactos</a>
+  </li>
+  
+</ul>
+  <!--categorias-->
 
 </nav>
+
 </header>
